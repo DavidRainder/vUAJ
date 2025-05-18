4fb064357c8add2fe3db8a4c646b5417626558a0
efeliua <100871802+efeliua@users.noreply.github.com>
2025-05-18 18:21:40 +0200

    Refactorización HUD ahora con HUDManager :D
    LoadHUD carga la selección del jugador según prefab

--- vUAJ/Assets/HUDCustomization/Scripts/HUDCustom.cs
+++ vUAJ/Assets/HUDCustomization/Scripts/HUDCustom.cs
@@ -14,7 +14,6 @@ public class HUDCustom : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField]
     private GameObject panel;
-    [SerializeField]
     private GameObject defaultPrefab;
     private GameObject defaultHUD;
     private GameObject customHUD;
@@ -34,102 +33,62 @@ public class HUDCustom : MonoBehaviour
     private GameObject customSettings;
     [SerializeField]
     private GameObject defaultFakeSettings;
-    Dictionary<string, ObjectInfo> HUDInfo;
 
-    float currentIconScale;
-    float currentShadowValue;
-
-
-    public struct ObjectInfo
-    {
-        public Vector3 baseScale;
-        public float scaleFactor;
-        public float shadowFactor;
-        public Vector3 pos;
-        public ObjectInfo(Vector3 scale, float scaleF, float shadow, Vector3 position)
-        {
-            baseScale = scale;
-            scaleFactor = scaleF;
-            shadowFactor = shadow;
-            pos = position;
-        }
-    }
     void Start()
     {
-        HUDInfo = new Dictionary<string, ObjectInfo>();
-        // Instantiate HUD prefab visualizer 
+        // Referencia HUD
+        defaultPrefab = HUDManager.Instance.getDefaultPrefab();
+
+        // Instancia HUD prefab visualizer 
+            // Default
+        Dictionary<string, HUDManager.ObjectInfo> HUDInfo = new Dictionary<string, HUDManager.ObjectInfo>();
         defaultHUD = Instantiate(defaultPrefab, panel.transform); 
         
         RectTransform overviewRect = panel.GetComponent<RectTransform>();
         RectTransform rect = defaultHUD.GetComponent<RectTransform>();
-
         foreach (Transform tr in defaultHUD.transform)
         {
-            RectTransform rectChild = tr.gameObject.GetComponent<RectTransform>();
-            UnityEngine.UI.DropShadow shadow = tr.gameObject.AddComponent<UnityEngine.UI.DropShadow>();
-
-            shadow.ShadowSpread = new Vector2(0.0f, 0.0f);
-            shadow.effectColor = Color.black;
-            shadow.EffectDistance = new Vector2(0f, 0f);
-            // rectChild.ForceUpdateRectTransforms();
-            HUDInfo.Add(tr.gameObject.name, new ObjectInfo(tr.localScale, 1, 0, rectChild.anchoredPosition3D));
+            HUDInfo.Add(tr.name, new HUDManager.ObjectInfo (tr.localScale, 1, 0, tr.position));
         }
+        HUDManager.Instance.saveConfig(HUDManager.HUDTypes.defaultHUD, HUDInfo); // Save
 
+
+            // Custom 
         customHUD = Instantiate(defaultPrefab, panel.transform);
+        applySavedToCustom(); // Load
+;
         rect = customHUD.GetComponent<RectTransform>();
         foreach (Transform tr in customHUD.transform)
         { 
             tr.gameObject.AddComponent<Drag>();
             RectTransform rectChild = tr.gameObject.GetComponent<RectTransform>();
-            UnityEngine.UI.DropShadow shadow = tr.gameObject.AddComponent<UnityEngine.UI.DropShadow>();
+            if (tr.gameObject.GetComponent<UnityEngine.UI.DropShadow>()== null)
+            {
+                UnityEngine.UI.DropShadow shadow = tr.gameObject.AddComponent<UnityEngine.UI.DropShadow>();
 
-            shadow.ShadowSpread = new Vector2(0.0f, 0.0f);
-            shadow.effectColor = Color.black;
-            shadow.EffectDistance = new Vector2(0f, 0f);
-            // rectChild.ForceUpdateRectTransforms();
+                shadow.ShadowSpread = new Vector2(0.0f, 0.0f);
+                shadow.effectColor = Color.black;
+                shadow.EffectDistance = new Vector2(0f, 0f);
+            }
         }
+
+        // Settings iniciales 
+        HUDManager.Instance.SetCurrentHUDType(HUDManager.HUDTypes.customHUD); // Se carga custom al entrar en menú 
+       // HUDManager.Instance.SetCurrentHUDType(HUDManager.HUDTypes.defaultHUD); 
         defaultHUD.SetActive(false);
         defaultFakeSettings.SetActive(false);
-        // Listeners
-        //presetDropdown.onValueChanged.AddListener(changePreset);
-        scaleSlider.onValueChanged.AddListener(sliderScale);
-        shadowSlider.onValueChanged.AddListener(sliderShadow);
     }
-
-    public void saveConfig()
+    public void saveCustom()
     {
-        string totalInfo = "{\n"; 
-            for (int i = 0; i<customHUD.transform.childCount; i++)
-            {
-                Transform tr = customHUD.transform.GetChild(i);
-                ObjectInfo aux = HUDInfo[tr.gameObject.name]; 
-                aux.scaleFactor = scaleSlider.value / 100;
-                aux.shadowFactor = shadowSlider.value;
-                aux.pos = tr.GetComponent<RectTransform>().anchoredPosition3D;  
-                HUDInfo[tr.gameObject.name] = aux;
-                string info = JsonUtility.ToJson(aux);
-                if (i == customHUD.transform.childCount - 1)
-                {
-                   totalInfo += "\"" + tr.gameObject.name + "\":" + info + "\n";
-                    
-                }
-                else
-                {
-                totalInfo += "\"" + tr.gameObject.name + "\":" + info + ",\n";
-                }
-            }
-            System.IO.File.WriteAllText(Application.persistentDataPath + "/customHUD.json",totalInfo+ " \n}");
-
-        //prueba de lectura json 
-        //GameObject aux2 = Instantiate(defaultPrefab, panel.transform);
-        //customHUD.SetActive(false); defaultHUD.SetActive(false);
-        //applyCustomToHUDPrefab(ref aux2);
+        HUDManager.Instance.SetCurrentHUDType(HUDManager.HUDTypes.customHUD);
+        HUDManager.Instance.saveConfig(HUDManager.HUDTypes.customHUD, null, customHUD);
     }
-
     public void sliderScale(System.Single value)
     {
-        scaleNumber.text = scaleSlider.value.ToString() + "%";
+        scaleNumber.text = ((int)scaleSlider.value).ToString() + "%";
         scaleItems(scaleSlider.value);
+        HUDManager.Instance.scaleFactor = scaleSlider.value / 100;
+
     }
     void scaleItems(float scale)
     {
@@ -137,17 +96,16 @@ public class HUDCustom : MonoBehaviour
         {
             //child.GetComponent<RectTransform>().localScale *= 0.5f;
             RectTransform rectChild = tr.gameObject.GetComponent<RectTransform>();
-            rectChild.localScale = HUDInfo[tr.gameObject.name].baseScale * scale/100;
-            Debug.Log(scale);
-            Debug.Log(rectChild.localScale);
+            rectChild.localScale = HUDManager.Instance.getCustomObjBaseScale(tr.gameObject.name) * scale/100;
             rectChild.ForceUpdateRectTransforms();
         }
 
     }
     public void sliderShadow(System.Single value)
     {
-        shadowNumber.text = shadowSlider.value.ToString() + "%";
+        shadowNumber.text = ((int)shadowSlider.value).ToString() + "%";
         setShadows(shadowSlider.value);
+        HUDManager.Instance.shadowFactor = shadowSlider.value;
     }
     void setShadows(float intensity)
     {
@@ -173,6 +131,7 @@ public class HUDCustom : MonoBehaviour
             defaultHUD.SetActive(false);
             customSettings.SetActive(true);
             defaultFakeSettings.SetActive(false);
+            HUDManager.Instance.SetCurrentHUDType(HUDManager.HUDTypes.customHUD);
         }
         else
         {
@@ -180,69 +139,37 @@ public class HUDCustom : MonoBehaviour
             defaultHUD.SetActive(true);
             customSettings.SetActive(false);
             defaultFakeSettings.SetActive(true);
+            HUDManager.Instance.SetCurrentHUDType(HUDManager.HUDTypes.defaultHUD);
+
+        }
+    }
+    void applySavedToCustom()
+    {
+        if (!HUDManager.Instance.applySavedConfigToHUD(customHUD, HUDManager.HUDTypes.customHUD))
+        {
+            Debug.Log("Custom settings not found, loading default as base :)");
+        }
+        else
+        {
+            initSliders(); // Set sliders a valor correspondiente
         }
     }
     public void resetCustom()
     {
-        foreach (Transform tr in customHUD.transform)
-        {
-            RectTransform rectChild = tr.gameObject.GetComponent<RectTransform>();
-            rectChild.anchoredPosition3D = HUDInfo[tr.gameObject.name].pos;
-        }
-        shadowSlider.value = shadowSlider.minValue; scaleSlider.value = 100; 
-        shadowNumber.text = shadowSlider.value + "%"; scaleNumber.text = 100+ "%";
+       applySavedToCustom();
     }
-    string ExtractJsonObject(string fullJson, string key)
+
+    public void initSliders()
     {
-        int keyIndex = fullJson.IndexOf($"\"{key}\"");
-        if (keyIndex == -1) return null;
+        shadowSlider.value = HUDManager.Instance.shadowFactor; 
+        scaleSlider.value = HUDManager.Instance.scaleFactor * 100;
+        shadowNumber.text = (int)shadowSlider.value + "%"; scaleNumber.text = (int)scaleSlider.value + "%";
 
-        int braceStart = fullJson.IndexOf('{', keyIndex);
-        if (braceStart == -1) return null;
-
-        int braceCount = 0;
-        for (int i = braceStart; i < fullJson.Length; i++)
-        {
-            if (fullJson[i] == '{') braceCount++;
-            else if (fullJson[i] == '}') braceCount--;
-
-            if (braceCount == 0)
-            {
-                int braceEnd = i;
-                return fullJson.Substring(braceStart, braceEnd - braceStart + 1);
-            }
-        }
-
-        return null;
+        // Listeners
+        scaleSlider.onValueChanged.AddListener(sliderScale);
+        shadowSlider.onValueChanged.AddListener(sliderShadow);
     }
-    public void applyCustomToHUDPrefab(ref GameObject HUDPrefab)
-    {
-        string path = Path.Combine(Application.persistentDataPath, "customHUD.json");
-        
-        if (File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            foreach (Transform tr in HUDPrefab.transform)
-            {
-                string obj = ExtractJsonObject(json, tr.gameObject.name);
 
-                if (!string.IsNullOrEmpty(obj))
-                {
-                    ObjectInfo data = JsonUtility.FromJson<ObjectInfo>(obj);
-                    RectTransform rect = tr.GetComponent<RectTransform>();
-                    rect.anchoredPosition3D = data.pos;
-                    rect.localScale = data.baseScale * data.scaleFactor;
-                    UnityEngine.UI.DropShadow shadow = tr.gameObject.AddComponent<UnityEngine.UI.DropShadow>();
-                    shadow.effectColor = Color.black;
-                    shadow.ShadowSpread = new Vector2(data.shadowFactor * 1.5f, data.shadowFactor * 1.5f);
-                }
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"Custom HUD no guardado en {path}, se mantiene el default");
-        }
-    }
     // Update is called once per frame
     void Update()
     {
---
+++ vUAJ/Assets/HUDCustomization/Scripts/HUDManager.cs
@@ -0,0 +1,172 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using static HUDCustom;
+
+public class HUDManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private GameObject HUDprefab;
+    public float scaleFactor;
+    public float shadowFactor;
+
+    Dictionary<string, ObjectInfo> HUDInfo; // Custom HUD info
+
+    public enum HUDTypes { defaultHUD, customHUD };
+
+    HUDTypes currentSelection;
+
+    static private HUDManager _instance;
+    public static HUDManager Instance { get { return _instance; } }
+
+    public struct ObjectInfo
+    {
+        public Vector3 baseScale;
+        public float scaleFactor;
+        public float shadowFactor;
+        public Vector3 pos;
+        public ObjectInfo(Vector3 scale, float scaleF, float shadow, Vector3 position)
+        {
+            baseScale = scale;
+            scaleFactor = scaleF;
+            shadowFactor = shadow;
+            pos = position;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(this.gameObject);
+
+        HUDInfo = new Dictionary<string, HUDManager.ObjectInfo>();
+
+        // Default values for later customization
+        scaleFactor = 100.0f;
+        shadowFactor = 0.0f;
+        currentSelection = HUDTypes.defaultHUD;
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Vector3 getCustomObjBaseScale(string objName)
+    {
+        return HUDInfo[objName].baseScale;
+    }
+    public GameObject getDefaultPrefab()
+    {
+        return HUDprefab;
+    }
+    public void saveConfig(HUDTypes type, Dictionary<string, HUDManager.ObjectInfo> objInfo = null, GameObject currHUD = null)
+    {
+        if(objInfo != null) { HUDInfo = objInfo; }
+        if (currHUD == null) { currHUD = HUDprefab; }
+        string totalInfo = "{\n";
+        for (int i = 0; i < currHUD.transform.childCount; i++)
+        {
+            Transform tr = currHUD.transform.GetChild(i);
+            ObjectInfo aux = HUDInfo[tr.gameObject.name];
+            aux.scaleFactor = scaleFactor;
+            aux.shadowFactor = shadowFactor;
+            aux.pos = tr.GetComponent<RectTransform>().anchoredPosition3D;
+            HUDInfo[tr.gameObject.name] = aux;
+            string info = JsonUtility.ToJson(aux);
+            if (i == currHUD.transform.childCount - 1)
+            {
+                totalInfo += "\"" + tr.gameObject.name + "\":" + info + "\n";
+
+            }
+            else
+            {
+                totalInfo += "\"" + tr.gameObject.name + "\":" + info + ",\n";
+            }
+        }
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/"+ type.ToString()+".json", totalInfo + " \n}");
+
+        //prueba de lectura json 
+        //GameObject aux2 = Instantiate(defaultPrefab, panel.transform);
+        //customHUD.SetActive(false); defaultHUD.SetActive(false);
+        //applyCustomToHUDPrefab(ref aux2);
+    }
+    public bool applySavedConfigToHUD(GameObject HUDObject, HUDTypes type) // Default or custom
+    {
+        string path = Path.Combine(Application.persistentDataPath, type.ToString()+".json");
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            foreach (Transform tr in HUDObject.transform)
+            {
+                string obj = ExtractJsonObject(json, tr.gameObject.name);
+
+                if (!string.IsNullOrEmpty(obj))
+                {
+                    ObjectInfo data = JsonUtility.FromJson<ObjectInfo>(obj);
+                    RectTransform rect = tr.GetComponent<RectTransform>();
+                    rect.anchoredPosition3D = data.pos;
+                    rect.localScale = data.baseScale * data.scaleFactor;
+                    UnityEngine.UI.DropShadow shadow = tr.gameObject.AddComponent<UnityEngine.UI.DropShadow>();
+                    shadow.effectColor = Color.black;
+                    shadow.ShadowSpread = new Vector2(data.shadowFactor * 1.5f, data.shadowFactor * 1.5f);
+                    if (type == HUDTypes.customHUD)
+                    {
+                        HUDInfo[tr.gameObject.name] = data; //shadow spread entre 1.5?
+                        scaleFactor = data.scaleFactor;
+                        shadowFactor = data.shadowFactor;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"{type.ToString()} HUD no guardado en {path}, se mantiene el default");
+            return false;
+        }
+        return true;
+    }
+
+    public void SetCurrentHUDType(HUDTypes type)
+    {
+        currentSelection = type;
+    }
+    string ExtractJsonObject(string fullJson, string key)
+    {
+        int keyIndex = fullJson.IndexOf($"\"{key}\"");
+        if (keyIndex == -1) return null;
+
+        int braceStart = fullJson.IndexOf('{', keyIndex);
+        if (braceStart == -1) return null;
+
+        int braceCount = 0;
+        for (int i = braceStart; i < fullJson.Length; i++)
+        {
+            if (fullJson[i] == '{') braceCount++;
+            else if (fullJson[i] == '}') braceCount--;
+
+            if (braceCount == 0)
+            {
+                int braceEnd = i;
+                return fullJson.Substring(braceStart, braceEnd - braceStart + 1);
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Load HUD segun selección del player en Menú
+    /// </summary>
+    void loadCurrentHUD()
+    {
+        applySavedConfigToHUD(HUDprefab, currentSelection); // Default or custom
+    }
+}
